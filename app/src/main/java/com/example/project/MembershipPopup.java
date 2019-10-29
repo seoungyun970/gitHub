e2d1f3c654b1e420f7f6ebd7b5b7c171ac278f1b
wo9374 <wo9374@naver.com>
2019-10-29 12:18:37 +0900

    프로필사진 선택 클릭시 다이얼로그창 뛰우고 앨범사진뛰우기 아직 사진자르기는 안댐 빡침 1스택 카메라도 아직안댐 빡침 2스택 구조변경후에도 카메라 안댐

--- app/src/main/java/com/example/project/MembershipPopup.java
+++ app/src/main/java/com/example/project/MembershipPopup.java
@@ -1,17 +1,20 @@
 package com.example.project;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.View;
@@ -22,7 +25,10 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -33,14 +39,16 @@ import com.google.firebase.database.DatabaseReference;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.net.URI;
 
 public class MembershipPopup extends Activity implements View.OnClickListener {
     private static final int  PICK_FROM_CAMERA=0;
     private static final int  PICK_FROM_ALBUM=1;
-    private static final int  CROP_FROM_CAMERA=2;
+    private static final int  CROP_FROM_IMAGE=2;
 
     private static Uri mImageCaptureUri;
     private ImageView profileImgBtn;
+    private String absoultePath;
 
     FirebaseAuth firebaseAuth;
     DatabaseReference mDatabase;
@@ -99,7 +107,6 @@ public class MembershipPopup extends Activity implements View.OnClickListener {
         register_checkBtn.setOnClickListener(this);
 
         profileImgBtn=findViewById(R.id.profileImgBtn);
-        profileImgBtn.setOnClickListener(this);
     }
 /*
     public void setInsertMode(){
@@ -176,88 +183,71 @@ public class MembershipPopup extends Activity implements View.OnClickListener {
             //TODO
             registerUser();
         }
-
-        if(view.getId()==R.id.profileImgBtn){
-            DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    doTakePhotoAction();
-                }
-            };
-
-            DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    doTakeAlbumAction();
-                }
-            };
-
-            DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                }
-            };
-
-            new AlertDialog.Builder(this)
-                    .setTitle("업로드할 이미지 선택")
-                    .setPositiveButton("사진 촬영",cameraListener)
-                    .setNeutralButton("앨범 선택",albumListener)
-                    .setNegativeButton("취소",cancelListener)
-                    .show();
-        }
     }
-    private void doTakePhotoAction(){ //카메라 촬영 후 이미지 가져오기
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+    public void profilebtn(View view){ //프로필 선택사진 눌렸을때  앨범선택 사진촬영 알림창 표시
+        DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                doTakePhotoAction();
+            }
+        };
+
+        DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                doTakeAlbumAction();
+            }
+        };
+
+        DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        };
+
+        new AlertDialog.Builder(this)   //프로필 알림창 표시
+                .setTitle("업로드할 이미지 선택")
+                .setPositiveButton("사진 촬영",cameraListener)
+                .setNeutralButton("앨범 선택",albumListener)
+                .setNegativeButton("취소",cancelListener)
+                .show();
+    }
+
+    public void doTakePhotoAction(){ //카메라 촬영 후 이미지 가져오기
+
+        Intent cintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
         //임시로 사용할 파일의 경로 생성
         String url="tmp_"+String.valueOf(System.currentTimeMillis())+".jpg";
         mImageCaptureUri= Uri.fromFile(new File(Environment.getExternalStorageDirectory(),url));
 
-        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT,mImageCaptureUri);
+        cintent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT,mImageCaptureUri);
+        startActivityForResult(cintent,PICK_FROM_CAMERA);
 
-        startActivityForResult(intent,PICK_FROM_CAMERA);
     }
-    private void doTakeAlbumAction(){ //앨범에서 이미지 가져오기
+    public void doTakeAlbumAction(){ //앨범에서 이미지 가져오기
         //앨범 호출
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
-        startActivityForResult(intent,PICK_FROM_ALBUM);
+        Intent aintent = new Intent(Intent.ACTION_PICK);
+        aintent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(aintent,PICK_FROM_ALBUM);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode != RESULT_OK)
             return;
 
         switch (requestCode){
-            case CROP_FROM_CAMERA:
-            {
-                //크롭이 된 이후의 이미지를 넘겨 받음
-                //이미지뷰에 이미지를 보여준다거나 부가적인 작업 이후에
-                //임시 파일을 삭제
-
-                final Bundle extras = data.getExtras();
-
-                if (extras != null){
-                    Bitmap photo = extras.getParcelable("data"); //크롭된 비트맵
-                    profileImgBtn.setImageBitmap(photo); //이미지 뷰에 크롭된 비트맵을 보여줌
-                }
-
-                //임시 파일 삭제
-                File f = new File(mImageCaptureUri.getPath());
-                if(f.exists()){
-                    f.delete();
-                }
-                break;
-            }
 
             case PICK_FROM_ALBUM:
             {
                 //이후의 처리가 카메라와 같으므로 일단 break없이 진행
                 mImageCaptureUri=data.getData();
+                Log.d("Dolbomi",mImageCaptureUri.getPath().toString());
             }
 
             case PICK_FROM_CAMERA:
@@ -265,20 +255,74 @@ public class MembershipPopup extends Activity implements View.OnClickListener {
                 // 이미지를 가져온 이후의 리사이즈할 이미지 크기를 결정
                 // 이후 이미지 크롭 어플리케이션을 호출
                 Intent intent=new Intent("com.android.camera.action.CROP");
-                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
                 intent.setDataAndType(mImageCaptureUri,"image/*");
 
-                //CROP할 이미지를 270*250 크기로 저장
-                intent.putExtra("outputX",100); //크롭한 이미지의 x축 크기
-                intent.putExtra("outputY",100); //크롭한 이미지의 y축 크기
+                intent.putExtra("outputX",80); //크롭한 이미지의 x축 크기
+                intent.putExtra("outputY",80); //크롭한 이미지의 y축 크기
                 intent.putExtra("aspectX",1); //크롭 박스의 x축 비율
                 intent.putExtra("aspectY",1); //크롭 박스의 y축 비율
                 intent.putExtra("scale",true);
                 intent.putExtra("return-data",true);
-                startActivityForResult(intent,CROP_FROM_CAMERA); //CROP_FROM_CAMERA case 문 이동
+                startActivityForResult(intent,CROP_FROM_IMAGE); //CROP_FROM_CAMERA case 문 이동
                 break;
             }
 
+            case CROP_FROM_IMAGE:
+            {
+                //크롭이 된 이후의 이미지를 넘겨 받음
+                //이미지뷰에 이미지를 보여준다거나 부가적인 작업 이후에
+                //임시 파일을 삭제
+
+                if(resultCode != RESULT_OK){
+                    return;
+                }
+
+                final Bundle extras = data.getExtras();
+
+                String filePath=Environment.getExternalStorageDirectory().getAbsolutePath()+"/Dolbomi/"+System.currentTimeMillis()+".jpg";
+
+                if (extras != null){
+                    Bitmap photo = extras.getParcelable("data"); //크롭된 비트맵
+                    profileImgBtn.setImageBitmap(photo); //이미지 뷰에 크롭된 비트맵을 보여줌
+
+                    storeCropImage(photo,filePath); //크롭된 이미지를 외부저장소,앨범에 저장
+                    absoultePath = filePath;
+                    break;
+                }
+
+                //임시 파일 삭제
+                File f = new File(mImageCaptureUri.getPath());
+                if(f.exists()){
+                    f.delete();
+                }
+            }
+        }
+
+    }
+
+    /*
+    * 비트맵을 저장하는 부분
+     */
+    private void storeCropImage(Bitmap bitmap,String filePath){
+        String dirPath=Environment.getExternalStorageDirectory().getAbsolutePath()+"/Dolbomi";
+        File directory_Dolbomi=new File(dirPath);
+
+        if(!directory_Dolbomi.exists()) // Dolbomi디렉토리에 폴더가 없다면 (새로 이미지를 저장할 경우)
+            directory_Dolbomi.mkdir();
+
+        File copyFile=new File(filePath);
+        BufferedOutputStream out=null;
+
+        try {
+            copyFile.createNewFile();
+            out=new BufferedOutputStream(new FileOutputStream(copyFile));
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,out);
+
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(copyFile)));
+            out.flush();
+            out.close();
+        } catch (Exception e){
+            e.printStackTrace();
         }
     }
 
