5ff2d6a3802a9a1277b1f14ce41998e51f99f167
seoungyun <gaguree95@naver.com>
2019-11-06 16:30:03 +0900

    날짜 스피너 / 식단표 레이아웃작성 2부

--- app/src/main/java/com/example/project/TeacherEattingItem.java
+++ app/src/main/java/com/example/project/TeacherEattingItem.java
@@ -2,7 +2,10 @@ package com.example.project;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -16,12 +19,13 @@ import java.util.Date;
 import java.util.Locale;
 
 public class TeacherEattingItem extends AppCompatActivity implements TimePicker.OnTimeChangedListener{
+    Button teacherEattingItem;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.teacher_eatting_item);
         Calendar c = Calendar.getInstance();
-
+        teacherEattingItem=findViewById(R.id.teacherEattingItem);
         DatePickerDialog datePickerDialog = new DatePickerDialog(TeacherEattingItem.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth, new DatePickerDialog.OnDateSetListener() {
 
 
@@ -71,5 +75,14 @@ public class TeacherEattingItem extends AppCompatActivity implements TimePicker.
     @Override
     public void onTimeChanged(TimePicker timePicker, int i, int i1) {
 
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.teacherEattingItem:
+            Intent teacherItemButton=new Intent(TeacherEattingItem.this,TeacherEatting.class);
+            startActivity(teacherItemButton);
+            break;
+        }
     }
 }
